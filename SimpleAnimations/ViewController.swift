b3a82f4cf7125e1ed4b94b70e16b84467aa380eb
Lesia <lesia.feliz@gmail.com>
2020-09-20 11:45:04 +0500

    Initial Commit

---
+++ SimpleAnimations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleAnimations
+//
+//  Created by Олеся Данилина on 20.09.2020.
+//  Copyright © 2020 Lesia Danilina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
